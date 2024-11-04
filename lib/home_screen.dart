739f66c41a805066b03a3b169b0e4930014b3c38
Avan <aliyakbiarov2.0@mail.ru>
2024-11-04 03:06:27 +0300

    first commit

---
+++ lib/home_screen.dart
@@ -0,0 +1,179 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_firebase_auth/account_screen.dart';
+import 'package:flutter_firebase_auth/login_screen.dart';
+
+final darktheme = ThemeData(
+  primaryColor: const Color.fromARGB(255, 248, 236, 128),
+  textTheme: const TextTheme(
+    bodyMedium: TextStyle(
+      color: Colors.white,
+      fontWeight: FontWeight.w500,
+      fontSize: 20,
+    ),
+    bodySmall: TextStyle(
+      color: Color.fromARGB(110, 255, 255, 255),
+      fontWeight: FontWeight.w700,
+      fontSize: 14,
+    ),
+  ),
+  scaffoldBackgroundColor: const Color.fromARGB(66, 80, 80, 80),
+  appBarTheme: const AppBarTheme(
+    backgroundColor: Color.fromARGB(66, 114, 114, 114),
+    titleTextStyle: TextStyle(
+      color: Colors.white,
+      fontSize: 25,
+      fontWeight: FontWeight.w700,
+    ),
+  ),
+  dividerColor: Colors.white12,
+  listTileTheme: const ListTileThemeData(iconColor: Colors.white),
+);
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final user = FirebaseAuth.instance.currentUser;
+
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('Добро пожаловать!'),
+      ),
+      body: SafeArea(
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                darktheme.scaffoldBackgroundColor.withOpacity(0.8),
+                const Color.fromARGB(66, 114, 114, 114).withOpacity(0.8),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Card(
+                    elevation: 8,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    color: darktheme.scaffoldBackgroundColor,
+                    child: Container(
+                      padding: const EdgeInsets.all(20),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        gradient: LinearGradient(
+                          colors: [
+                            darktheme.primaryColor.withOpacity(0.9),
+                            const Color.fromARGB(110, 255, 255, 255),
+                          ],
+                          begin: Alignment.topLeft,
+                          end: Alignment.bottomRight,
+                        ),
+                      ),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          if (user == null) ...[
+                            // Текст для незарегистрированных пользователей
+                            Text(
+                              "Вы скачали приложение для отслеживания актуального курса криптовалют!",
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .bodyMedium
+                                  ?.copyWith(
+                                    fontSize: 24,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                              textAlign: TextAlign.center,
+                            ),
+                            const SizedBox(height: 20),
+                            // Изображение с округлением
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(15),
+                              child: Image.asset(
+                                'assets/images/crypto_image.jpeg', // Укажите путь к вашему изображению
+                                height: 200,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ] else ...[
+                            // Текст для зарегистрированных пользователей
+                            Text(
+                              'Вы скачали приложение для отслеживания актуального курса криптовалют!!',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .bodyMedium
+                                  ?.copyWith(
+                                    fontSize: 24,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                              textAlign: TextAlign.center,
+                            ),
+                            const SizedBox(height: 20),
+                            Text(
+                              'Добро пожаловать, ${user.email}!',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .bodySmall
+                                  ?.copyWith(
+                                    fontSize: 16,
+                                  ),
+                              textAlign: TextAlign.center,
+                            ),
+                          ],
+                        ],
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                      height: 20), // Отступ между контейнером и кнопкой
+                  ElevatedButton(
+                    onPressed: () {
+                      if ((user == null)) {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const LoginScreen()),
+                        );
+                      } else {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const AccountScreen()),
+                        );
+                      }
+                    },
+                    style: ElevatedButton.styleFrom(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 40, vertical: 15),
+                      backgroundColor: darktheme.primaryColor, // Цвет кнопки
+                      shape: RoundedRectangleBorder(
+                        borderRadius:
+                            BorderRadius.circular(10), // Округленные углы
+                      ),
+                    ),
+                    child: const Text(
+                      'Начать отслеживание',
+                      style:
+                          TextStyle(fontSize: 20), // Увеличенный размер текста
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_firebase_auth/account_screen.dart';
+import 'package:flutter_firebase_auth/firebase_streem.dart';
+import 'package:flutter_firebase_auth/home_screen.dart';
+import 'package:flutter_firebase_auth/login_screen.dart';
+import 'package:flutter_firebase_auth/reset_password_screen.dart';
+import 'package:flutter_firebase_auth/signup.dart.dart';
+import 'package:flutter_firebase_auth/verify_email_screen.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: darktheme.copyWith(
+        pageTransitionsTheme: const PageTransitionsTheme(builders: {
+          TargetPlatform.android: CupertinoPageTransitionsBuilder(),
+        }),
+      ),
+      routes: {
+        '/': (context) => const FirebaseStream(),
+        '/home': (context) => const HomeScreen(),
+        '/account': (context) => const AccountScreen(),
+        '/login': (context) => const LoginScreen(),
+        '/signup': (context) => const SignUpScreen(),
+        '/reset_password': (context) => const ResetPasswordScreen(),
+        '/verify_email': (context) => const VerifyEmailScreen(),
+      },
+      initialRoute: '/',
+    );
+  }
+}
